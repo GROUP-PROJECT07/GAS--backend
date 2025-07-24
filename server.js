e6119834944c6a28f5cf165d9d5f7e77d19b6161
Kimathi Sedegah <sedkim370@gmail.com>
2025-07-24 19:31:59 +0000

    server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Init Supabase client
+
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// Setup multer for memory storage (no need to save locally)
+// Setup multer for memory storage 
 const upload = multer({ storage: multer.memoryStorage() });
 
 // POST endpoint to upload file
@@ -32,7 +32,6 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     return res.status(500).json({ error });
   }
 
-  // Optional: return the public URL
   const { data: publicUrlData } = supabase
     .storage
     .from('correspondence-files')
